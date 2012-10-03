4e9a55a5b48d6cde8344b94fa0cd6ffbc1b24747
Tax <paultax@gmail.com>
2012-10-03 15:30:27 +0200

    creating python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='requests-aws',
+    version='0.1.0',
+    author='Paul Tax',
+    author_email='paultax@gmail.com',
+    #packages=['requests-aws'],
+    url='https://github.com/tax/python-requests-aws',
+    license='BSD licence, see LICENCE.txt',
+    description='AWS authentication for Amazon S3 for the python requests module',
+    long_description=open('README.md').read(),
+)
